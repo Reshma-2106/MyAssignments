65d75d322398cc9c823e3751a79e5b2c4b2c453e
Reshma-2106 <reshma.21061995@gmail.com>
2025-12-03 21:39:33 +0530

    Add files via upload
    Week1Day1

---
+++ Week1/day1/EmployeeDetails.java
@@ -0,0 +1,27 @@
+package Week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name = "John Deo";
+		int age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(gender);
+		System.out.println(acceptTerms);
+		System.out.println(rating);
+		System.out.println(mobileNumber);
+		System.out.println(pinCode);
+		System.out.println(luckyNumber);
+
+	}
+
+}
