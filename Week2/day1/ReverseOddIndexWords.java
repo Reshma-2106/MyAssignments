4996671c57d2e09a1a5178767311132d7ab4cab9
Reshma-2106 <reshma.21061995@gmail.com>
2025-12-09 20:49:54 +0530

    Update ReverseOddIndexWords.java
    added inline comments

--- Week2/day1/ReverseOddIndexWords.java
+++ Week2/day1/ReverseOddIndexWords.java
@@ -4,20 +4,28 @@ public class ReverseOddIndexWords {
 
 	public static void main(String[] args) {
 		String test = "I am a software tester";
+		//split the words and store in array
 		String[] words = test.split(" ");
+		//Traverse through each word
 		for (int i = 0; i < words.length; i++) {
+			//find the odd index
 			if (i % 2 == 1) {
+				//Convert the word into character array
 				char[] ch = words[i].toCharArray();
+				//reverse using loop
 				for (int j = ch.length - 1; j >= 0; j--) {
 					System.out.print(ch[j]);
 				}
 				
 			}else {
+				//Print even position words directly
 				 System.out.print(words[i]);
 			}
+			//Print space between words
 			 System.out.print(" ");
 		}
 
 	}
 
 }
+
